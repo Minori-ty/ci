62ec85328d1c5a29cf749bc2760ddd3bf7f38b77
守夜人 <1229248506@qq.com>
2022-03-09 20:37:52 +0800

    img

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,11 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-    <img src="./assets/logo.png" alt="" srcset="" />
+    <img
+        src="https://p26-passport.byteacctimg.com/img/user-avatar/d5471d5ab20ade887cac0d4d83f2765e~300x300.image"
+        alt=""
+        srcset=""
+    />
     <h1>CI/CD</h1>
     <p>这是测试的内容</p>
 </template>
